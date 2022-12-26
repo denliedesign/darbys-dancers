2dd9af45fc6921a1c429a7eae835ce5065c465b8
denliedesign <customdenlie@gmail.com>
2022-12-26 15:10:26 -0600

    dash link fix 2

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -32,7 +32,7 @@
             </li>
             <li class="nav-item mx-4">
                 <small>
-                    <a href="{{ route('dashboard') }}" class="text-decoration-none brand-txt-logo-blue">Dashboard</a>
+                    <a href="/dashboard" class="text-decoration-none brand-txt-logo-blue">Dashboard</a>
                 </small>
             </li>
         @endguest
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('/people');
+    return view('dashboard');
 //})->middleware(['auth', 'verified'])->name('dashboard');
 })->middleware(['auth', 'verified', 'subscribed'])->name('dashboard');
 
