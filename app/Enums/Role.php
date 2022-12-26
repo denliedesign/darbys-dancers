9a2aac4319aee446feca3e88d91bab222c5a9426
denliedesign <customdenlie@gmail.com>
2022-12-26 14:56:34 -0600

    switch role again and remove space

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -2,14 +2,13 @@
 
 namespace App\Enums;
 
-//enum Role: string
-//{
-//    case Admin = 'Admin';
-//    case Author = 'Author';
-//}
-
-class Role
-{
-    const Admin = 'Admin';
-    const Author = 'Author';
+enum Role:string {
+    case Admin = 'Admin';
+    case Author = 'Author';
 }
+
+//class Role
+//{
+//    const Admin = 'Admin';
+//    const Author = 'Author';
+//}
