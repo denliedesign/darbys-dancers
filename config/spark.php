d7835fb529467fe2a9d8bd3a1f5ca7bc76d2e890
denliedesign <customdenlie@gmail.com>
2024-01-25 16:32:53 -0600

    remove the commented out plans

--- config/spark.php
+++ config/spark.php
@@ -137,24 +137,6 @@ return [
             'default_interval' => 'yearly',
 
             'plans' => [
-//                [
-//                    'name' => 'Darby\'s Dancers Membership Test',
-//                    'short_description' => 'Testing Studio membership to Darby\'s Dancers program.',
-//                    'yearly_id' => 'price_1MGk2NKrXc5EOpOwIrZTHshf',
-//                    'features' => [],
-//                ],
-//                [
-//                    'name' => 'Darby\'s Free Dancers Membership Test',
-//                    'short_description' => 'Testing Free Studio membership to Darby\'s Dancers program.',
-//                    'yearly_id' => 'price_1OcWa3KrXc5EOpOwbac3JuGf',
-//                    'features' => [],
-//                ],
-//                [
-//                    'name' => 'Full Darby\'s Dancers Membership',
-//                    'short_description' => 'Full Studio membership to Darby\'s Dancers program.',
-//                    'yearly_id' => 'price_1MMQFpKrXc5EOpOwkvE8qMgT',
-//                    'features' => [],
-//                ],
                 [
                     'name' => 'Darby\'s Dancers Membership',
                     'short_description' => 'By starting your subscription today at $0, you agree to be automatically charged $495 annually, beginning on September 1st each year, for a studio membership to Darby\'s Dancers program.',
